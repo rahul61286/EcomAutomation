8cbbc088f7148b81ec7050f73486067533cc2f00
rahul <rahul.singhdec06@gmail.com>
2025-04-20 02:59:25 +0530

    cart new changes JIRA 2134

--- src/main/java/ecomAutomation/CartPage.java
+++ src/main/java/ecomAutomation/CartPage.java
@@ -40,7 +40,6 @@ public class CartPage extends AbstractComponent{
 	public CheckOutPage goToCheckOut() throws InterruptedException
 	{
 		Thread.sleep(2000);
-		System.out.println("Added method to cover delay");
 		checkout.click();
 		return new CheckOutPage(driver);
 		
